b9dd763f358d08838202cb0c718aca7b31f6bf42
nigel_bree <nigel_bree@localhost>
2012-03-10 12:11:26 +1300

    Add a new utility to try and probe whether a port is open on a host.
    This exists for the benefit of users in South Africa, where evidently it's
    relatively commonplace to have dual ISP connections (where that's largely
    unknown in AU and NZ). This means that the existing GAE service to detect
    the user's ISP may be correct, but if a user has a static route for an ISP
    like WebAfrica then it will detect their "other" ISP as the primary.
    
    WebAfrica don't have any customer-only HTTP services that I can probe for in
    the existing script, but they *do* have a customer-only NTTP server. There's
    no way to test that using scriptable components (the Collaboration Data
    Object system has an NNTP client that can post to, but not read from, NNTP)
    so I'm adding this small app to start being able to return additional probe
    sequences from the GAE service to the setfilter.js script.
    
    That can then in turn call out to this app; one specific thing that the app
    copes with is the existence of the Avast! antivirus program; it contains a
    set of transparent proxies for HTTP, SMTP and NNTP traffic, and the proxy is
    good at interfering with connections to NNTP servers - they always succeed
    but then fail later, after about 10 seconds while the proxy tries the target
    on the TLS and then plain NNTP ports. So if Avast! is present, the probe app
    has to actually try reading from the connected socket to see whether there's
    a 400 response sent.
    
    Currently I'm testing how useful this is, with an eye to then working on the
    script and webservice side to integrate this into the regular process in a
    5.4.0 release once that works. Having got this far I am considering whether
    it might work for helping to evaluate whether an ISP is providing Steam HTTP
    content or not (as TelstraClear do not, they have port 80 closed on their
    Steam content server), but to be really useful I'd have to get some kind of
    indication sent to the webservice about which way the test result fell so
    even though such a thing would clearly be hugely beneficial to users I can
    expect that it'd be resisted.

---
+++ probe/probe.cpp
@@ -0,0 +1,232 @@
+/**@addtogroup Probe Test whether a given port on a host is connectable.
+ * @{@file
+ *
+ * This application attempts to connect to a server and port, and just returns
+ * the status as a result to the invoker.
+ *
+ * @author Nigel Bree <nigel.bree@gmail.com>
+ *
+ * Copyright (C) 2011 Nigel Bree; All Rights Reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+/**
+ * Simple command-line argument extraction, about as unsophisticated as it can
+ * possibly get.
+ */
+
+wchar_t * split (wchar_t * args) {
+        if (args == 0)
+                return args;
+
+        /*
+         * The argument is quoted (which is typical of the first argument, the
+         * program name, because of the need to avoid problems with spaces in
+         * the path), in which case we skip to the ending quote first.
+         */
+
+        if (* args == '"') {
+                for (;;) {
+                        wchar_t         ch = * args;
+                        if (ch == '"')
+                                break;
+
+                        if (ch == 0)
+                                return 0;
+                        ++ args;
+                }
+        }
+
+        /*
+         * Split at the next space.
+         */
+
+        for (;;) {
+                wchar_t         ch = * args;
+                if (ch == ' ')
+                        break;
+
+                if (ch == 0)
+                        return 0;
+                ++ args;
+        }
+
+        * args = 0;
+        ++ args;
+
+        /*
+         * If there are additional spaces, consume them.
+         */
+
+        while (* args == ' ')
+                ++ args;
+
+        return args;
+}
+
+/**
+ * If we're asked to probe for an NTTP port, then sometimes we have to deal
+ * with local proxies.
+ *
+ * In this case, we actually try and read from the socket to at least get the
+ * server's initial hello. For the annoying Avast! proxy, that at least does
+ * not get sent until the real target responds to the proxy, and if the proxy
+ * connection doesn't work (after 5-6 seconds, since it tries the TLS version
+ * of the port even if we connected plain-text) then it spits a 400 out.
+ */
+
+int checkNntp (SOCKET s) {
+        char            buf [128];
+        int             result;
+        result = recv (s, buf, sizeof (buf), 0);
+        if (result < 5)
+                return 1;
+
+        /*
+         * Various tedious socket-isms can apply, such as the bytes for the
+         * initial response code trickling in over time. Let's not worry
+         * about that, just deal with the basics.
+         */
+
+        void          * end = memchr (buf, ' ', result);
+        if (end == 0)
+                return 1;
+
+        return memcmp (buf, "400", 3) == 0 ? 1 : 0;
+}
+
+/**
+ * Probe for the indicated port at the given host.
+ */
+
+int probe (wchar_t * host, wchar_t * port) {
+
+        /*
+         * Detect the presence of the Avast! virus scanner; it includes a set
+         * of proxy-type firewalls that are somewhat tedious to deal with, and
+         * in the case of NNTP mean that NNTP connections go through their
+         * proxy; so, our connect will succeed (being locally looped back) and
+         * send us nothing while the proxy module slowly decides whether it can
+         * or can't connect to the real target over TLS or plain-text NNTP.
+         *
+         * So, if Avast! is present we can choose to either try and read from
+         * the socket once it connects (so we can get the response code, which
+         * will generally be 400 once the Avast proxy fails), or we can forget
+         * the whole probing process because that introduces too much delay.
+         */
+
+        HMODULE         avast = GetModuleHandle (L"snxhk.dll");
+
+        WSADATA         wsaData;
+        if (WSAStartup (MAKEWORD (2, 2), & wsaData) != 0)
+                return 2;
+
+        SOCKET          s;
+        s = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (s == SOCKET_ERROR)
+                return 2;
+
+        sockaddr_in     any;
+        any.sin_family = AF_INET;
+        any.sin_port = 0;
+        any.sin_addr.S_un.S_addr = INADDR_ANY;
+        if (bind (s, (sockaddr *) & any, sizeof (any)) != 0)
+                return 2;
+
+        /*
+         * Look up the hostname and convert the port number into numeric form,
+         * all handily in one function.
+         */
+
+        ADDRINFOW     * address;
+        if (GetAddrInfoW (host, port, 0, & address) != 0)
+                return 2;
+
+        /*
+         * Just test whether the port is open or not.
+         */
+
+        int             result;
+        result = connect (s, address->ai_addr, address->ai_addrlen) ? 1 : 0;
+
+        /*
+         * Decide whether to actually wait for data, if we're dealing with the
+         * Avast! proxy being present on a system.
+         */
+
+        sockaddr_in   * addr = (sockaddr_in *) address->ai_addr;
+        switch (htons (addr->sin_port)) {
+        case 119:
+                if (avast && result == 0)
+                        result = checkNntp (s);
+                break;
+
+        case 80:
+        default:
+                break;
+        }
+
+        return result;
+}
+
+/**
+ * This is intended as a "naked" WinMain without the return Visual C++ run-time
+ * at all (not just avoiding the broken locale machinery).
+ */
+
+int CALLBACK myWinMain (void) {
+        HANDLE          err = GetStdHandle (STD_ERROR_HANDLE);
+        unsigned long   written = 0;
+
+        /*
+         * Since we're not using the regular C machinery, get and split the
+         * command line by hand. The CommandLineToArgvW () routine would do the
+         * normal conversion to C style for us, but that depends on SHELL32.DLL
+         * and we shouldn't need it.
+         */
+
+        wchar_t       * base = GetCommandLineW ();
+        wchar_t       * name = split (base);
+        wchar_t       * port = split (name);
+        wchar_t       * extra = split (port);
+        if (port == 0)
+                return 2;
+
+        int             result = probe (name, port);
+
+        if (extra == 0)
+                ExitProcess (result);
+
+static  const char      text [] = "Probe result: ";
+        WriteFile (err, text, strlen (text), & written, 0);
+
+        char            buf [2] = { '0' + result };
+        WriteFile (err, buf, 1, & written, 0);
+
+        ExitProcess (result);
+}
